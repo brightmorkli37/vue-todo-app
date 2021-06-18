b3b00c6744795dc28f5c84e285cd539609086381
brightmorkli <brightmorkli37@gmail.com>
2021-06-18 23:24:14 +0000

    version 1 complete

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>Taskie App</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, optio. Nihil tempore modi illum obcaecati ipsam rerum voluptatem corrupti architecto accusamus, cum nulla alias sed vitae, esse officia sit. Quasi.
+      Voluptates error iusto atque reprehenderit temporibus dolores rem accusantium sunt harum dolor eaque placeat quis, reiciendis aut a quo iste maiores! Qui officiis in similique aspernatur. Perspiciatis minus repellendus quam.
+      Quam earum quidem aut accusantium, est maiores repellendus, aliquam cupiditate beatae ab excepturi exercitationem fugit, et non ratione quod! Ratione aliquam culpa quibusdam deserunt recusandae vitae a incidunt, ducimus modi!
+      Cum, doloribus dolorem. Numquam dolorem fuga dolorum minus natus, molestias doloribus quos nesciunt unde dolor sapiente ipsa aut, ullam hic quaerat ad! Dignissimos odit nobis non repudiandae numquam illo voluptas!
+      Non veritatis voluptatem mollitia blanditiis harum error, sed omnis quos fuga autem ullam, consectetur deserunt beatae hic eveniet dolore, recusandae ea aliquam. Nulla tenetur eaque dolore placeat, eligendi sunt sit.
+      Vitae quis, hic eligendi nisi deleniti id modi sequi soluta quo esse inventore. Unde numquam delectus in, hic sit animi laborum voluptate consequuntur libero distinctio ullam consequatur sed, amet ab?
+    </p>
   </div>
 </template>
---
+++ src/views/Todo.vue
@@ -0,0 +1,106 @@
+<template>
+  <div>
+    <v-text-field
+            v-model="newTaskTitle"
+            @click:append="addTask"
+            @keyup.enter="addTask"
+            class="pa-3"
+            outlined
+            label="Add task"
+            append-icon="mdi-plus"
+            hide-details
+            clearable
+          ></v-text-field>
+
+     <v-list
+      flat
+    >
+
+        <div v-for="task in tasks" :key="task.id"
+        class="pt-0"
+        >
+          <v-list-item @click="doneTask(task.id)"
+          :class="{'light-green': task.done}"
+          > 
+>
+          <template v-slot:default>
+            <v-list-item-action>
+              <v-checkbox :input-value="task.done"></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title
+              :class="{'text-decoration-line-through': task.done}"
+              >
+                {{ task.title }}
+                </v-list-item-title>
+            </v-list-item-content>
+            <v-list-item-action>
+          <v-btn 
+          @click.stop="deleteTask(task.id)"
+          icon>
+            <v-icon color="primary lighten-1">mdi-delete</v-icon>
+          </v-btn>
+        </v-list-item-action>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+        </div>
+
+    </v-list>
+  </div>  
+</template>
+
+<script>
+
+  export default {
+    name: 'Home',
+
+    components: {
+    },
+    data () {
+      return {
+        newTaskTitle: '',
+        settings: [],
+        tasks: [
+          // {
+          //   id: 1,
+          //   title: 'todo item 1',
+          //   done: false
+          // },
+          // {
+          //   id: 2,
+          //   title: 'todo item 2',
+          //   done: false
+          // },
+          // {
+          //   id: 3,
+          //   title: 'todo item 3',
+          //   done: false
+          // }
+        ],
+      }
+    },
+    methods: {
+      addTask(){
+        let newTask = {
+          id: Date.now,
+          title: this.newTaskTitle,
+          done: false,
+        }
+        this.tasks.push(newTask)
+        this.newTaskTitle = ''
+      },
+
+      doneTask(id){
+        let task = this.tasks.filter(task => task.id === id)[0]
+        task.done = !task.done; 
+      },
+
+      deleteTask(id){
+      this.tasks = this.tasks.filter(task => task.id !== id)
+    }
+    },
+    
+  }
+</script>
